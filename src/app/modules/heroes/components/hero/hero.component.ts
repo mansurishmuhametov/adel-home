ab318c8d5a348de8fd2c1ec0efb58d12185e1caf
Mansur Ishmuhametov <mansurishmuhametov@gmail.com>
2019-07-13 23:12:18 +0300

    Изменена версия библиотек для тестов

--- src/app/modules/heroes/components/hero/hero.component.ts
+++ src/app/modules/heroes/components/hero/hero.component.ts
@@ -1,26 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core';
 import {
-    map,
-    filter,
-    debounceTime,
-    distinctUntilChanged,
-    switchMap,
-    catchError,
-    find,
-    buffer,
-    bufferTime,
-    take,
     mergeMap
 } from 'rxjs/operators';
-import { TargetLocator } from 'selenium-webdriver';
-import { timeout } from 'q';
 
 import { of } from 'rxjs/observable/of';
-import { from } from 'rxjs/observable/from';
-import { range } from 'rxjs/observable/range';
-import { interval } from 'rxjs/observable/interval';
-import { fromEvent } from 'rxjs/observable/fromEvent';
 
 @Component({
     selector: 'app-hero',
@@ -31,12 +14,6 @@ export class HeroComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        // of('Ell')
-        //     .subscribe((name) => {
-        //         of(`${name} Lad`)
-        //             .subscribe((full) => console.log(full));
-        //     });
-
         of('Ell').pipe(
             mergeMap(name => of(`${name} Lad`))
         ).subscribe(fullName => console.log(fullName));
