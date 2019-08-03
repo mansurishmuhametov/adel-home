815e6f2b46d9da546d5ecc9ca1239e70c18fa509
Mansur Ishmuhametov <mansurishmuhametov@gmail.com>
2019-08-03 12:29:52 +0300

    Добавлен переход на страницу 'Партнер'

--- src/app/modules/clients/components/footer/footer.component.ts
+++ src/app/modules/clients/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-    constructor() { }
+    constructor(
+        private router: Router
+    ) { }
 
     ngOnInit() {
     }
 
+    goTo(state: string) {
+        this.router.navigate([state]);
+    }
 }
