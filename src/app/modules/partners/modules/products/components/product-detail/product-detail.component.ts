dff6535ac2e2a01f7aa23e046e8ee947ed61f162
Mansur Ishmuhametov <mansurishmuhametov@gmail.com>
2019-08-11 20:15:37 +0300

    Изменен внешний вид страницы 'Редактирование товара'

--- src/app/modules/partners/modules/products/components/product-detail/product-detail.component.ts
+++ src/app/modules/partners/modules/products/components/product-detail/product-detail.component.ts
@@ -167,9 +167,6 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
     }
 
     public prioritySelected(priority: number): void {
-        // 07
-        debugger;
-
         this.updateForm.controls.priority.setValue(priority);
     }
 
@@ -199,5 +196,30 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
 
     private clear(): void {
         this.product = new Product(null, '', '', 0, 0, null, '', 0);
+    }
+
+
+    public imagePath;
+    imgURL: any;
+    public message: string;
+
+    public preview(files): void {
+        if (files.length === 0)
+            return;
+
+        const mimeType = files[0].type;
+
+        if (mimeType.match(/image\/*/) == null) {
+            this.message = "Только картинки";
+
+            return;
+        }
+
+        const reader = new FileReader();
+        this.imagePath = files;
+        reader.readAsDataURL(files[0]);
+        reader.onload = (_event) => {
+            this.imgURL = reader.result;
+        }
     }
 }
